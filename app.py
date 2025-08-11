1b1f56951489ac99cc62ac0fe5f3aaae29016619
mayli117 <mayli117@ms38.hinet.net>
2025-08-11 23:42:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ def calculate():
    payload = request.json
    result = calculate_fees(payload)
    return jsonify(result)
-if __name__ == '__main__':
-   app.run(debug=True)
+   if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
